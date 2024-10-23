e79fbe85006e4b87d0f3e37a10287997672ca66b
vkc01 <vincentkcarreon@gmail.com>
2024-10-23 15:22:12 -0700

    d

---
+++ Assets/Scenes/Selection.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UIElements;
+
+public class Selection : MonoBehaviour
+{
+    CardSelected = ;
+    void Update()
+    {
+        Keyboard
+    }
+}
