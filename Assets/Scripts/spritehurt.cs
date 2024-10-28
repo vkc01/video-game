495366519079380394b13a66e5df6a6398100833
vkc01 <vincentkcarreon@gmail.com>
2024-10-27 18:54:31 -0700

    spritehurt

--- Assets/Scripts/spritehurt.cs
+++ Assets/Scripts/spritehurt.cs
@@ -1,14 +1,22 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
-public class NewBehaviourScript : MonoBehaviour
+public class spritehurt : MonoBehaviour
 {
     [SerializeField] Image myImage;
     [SerializeField] Color myColor;
     void Start()
     {
         myColor.a = 1;
+    }
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.UpArrow))
+        {
+            ColorToDamage();
+        }
     }
 
     /*  place this in update in the caller script: FindAnyObjectByType<CALLERSCRIPTSNAME>().ColorToDamage(); */
