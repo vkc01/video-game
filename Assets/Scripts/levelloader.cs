155dd559e1c58ae14cfe034f0ea2969c5c295fee
vkc01 <vincentkcarreon@gmail.com>
2024-10-29 15:47:38 -0700

    g

--- Assets/Scripts/levelloader.cs
+++ Assets/Scripts/levelloader.cs
@@ -1,18 +1,32 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class levelloader : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    public Animator transition;
+    public float transitionTime = 1f;
 
-    // Update is called once per frame
+    // FindAnyObjectByType<CALLERSCRIPTSNAME>().GoToPlayfield();
+    //GoToPlayfield()
     void Update()
     {
-        
+        if (Input.GetMouseButtonDown(0))
+        {
+            LoadNextLevel();
+        }
+    }
+
+    public void LoadNextLevel()
+    {
+        StartCoroutine(LoadLevel(SceneManager.GetActiveScene().buildIndex + 1));
+    }
+
+    IEnumerator LoadLevel(int levelIndex)
+    {
+        transition.SetTrigger("Start");
+        yield return new WaitForSeconds(transitionTime);
+        SceneManager.LoadScene(levelIndex);
     }
 }
