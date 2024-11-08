b23de9682e81f8be47a2ec241802f9fe69f880e3
vkc01 <vincentkcarreon@gmail.com>
2024-11-07 19:36:47 -0800

    Update cardsattempt.cs

--- Assets/Scripts/cardsattempt.cs
+++ Assets/Scripts/cardsattempt.cs
@@ -14,22 +14,25 @@ public class cardsattempt : MonoBehaviour
     void Start()
     {
         howManyAdded = 0.0f;
-        gapFromOneItemToTheNextOne = 50.0f;
+        gapFromOneItemToTheNextOne = 75.0f;
     }
 
     public void FitCards()
     {
-        Debug.Log("working");
-        if (items.Count == 0) //if list is null, stop function
-            return;
+        while (howManyAdded < 10)
+        {
+            Debug.Log("working");
+            if (items.Count == 0) //if list is null, stop function
+                return;
 
-        Image img = items[0]; //Reference to first image in my list
+            Image img = items[0]; //Reference to first image in my list
 
-        img.transform.position = start.position; // Relocating my card to the Start Position
-        img.transform.position += new Vector3((howManyAdded * gapFromOneItemToTheNextOne), 0, 0); // Moving my card 1f to the right
-        img.transform.SetParent(HandDeck); // Setting my card parent to be the Hand Panel
+            img.transform.position = start.position; // Relocating my card to the Start Position
+            img.transform.position += new Vector3((howManyAdded * gapFromOneItemToTheNextOne), 0, 0); // Moving my card 1f to the right
+            img.transform.SetParent(HandDeck); // Setting my card parent to be the Hand Panel
 
-        items.RemoveAt(0);
-        howManyAdded++;
+            items.RemoveAt(0);
+            howManyAdded++;
+        }
     }
 }
