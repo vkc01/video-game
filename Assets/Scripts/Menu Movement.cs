e90d36fc69e783a9e2b5fb934d5f26f6a2c7a68d
vkc01 <vincentkcarreon@gmail.com>
2025-01-05 16:00:27 -0800

    r

--- Assets/Scripts/Menu Movement.cs
+++ Assets/Scripts/Menu Movement.cs
@@ -20,37 +20,57 @@ public class MenuMovement : MonoBehaviour
         if (wherePlayer == "title") // goes from title to main
         {
             allelements.SetTrigger("titlescreentomainscreen");
+            selector.SetTrigger("selector-titletomain");
             wherePlayer = "main";
             movingselector = 1;
-            selectorpos.anchoredPosition = new Vector2(-1.5f, 57);
+            return;
         }
         if (wherePlayer == "main")
         {
             if (whereSelector == "play") // main to play screen
             {
                 allelements.SetTrigger("mainscreentoplayscreen");
+                selector.SetTrigger("selector-maintoplay");
+                wherePlayer = "play";
+                MainToPlay();
+                return;
             }
             if (whereSelector == "credits") // main to credits screen
             {
                 allelements.SetTrigger("mainscreentocreditsscreen");
+                selector.SetTrigger("selector-maintocredits");
+                wherePlayer = "credits";
             }
             if (whereSelector == "help") // main to help screen
             {
                 allelements.SetTrigger("mainscreentohelpscreen");
+                selector.SetTrigger("selector-maintohelp");
+                wherePlayer = "help";
             }
             if (whereSelector == "title") // main to title screen
             {
                 allelements.SetTrigger("mainscreentotitlescreen");
-                selector.SetTrigger("titletoplay");
+                selector.SetTrigger("selector-maintotitle");
+                wherePlayer = "title";
                 movingselector = 0;
                 whereSelector = "play";
+                return;
             }
         }
+    }
+
+    void MainToPlay()
+    {
+        selector.SetTrigger("selector-TPtoplayer2");
+        whereSelector = "play";
     }
 
     // selector controller
     void OnOne(InputValue value)
     {
+        /*Debug.Log(selectorpos.anchoredPosition);
+        selectorpos.anchoredPosition = new Vector2(0, 0);
+        Debug.Log(selectorpos.anchoredPosition);*/
         if (movingselector == 0)
         {
             return;
